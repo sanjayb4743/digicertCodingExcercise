25cabd2405d1e6cf133e17c00faade262bbe3eda
sanjayb4743 <sanjayb4743@gmail.com>
2023-01-13 12:32:16 -0600

    Adding code changes for the Repository class

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import com.example.model.Book;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
